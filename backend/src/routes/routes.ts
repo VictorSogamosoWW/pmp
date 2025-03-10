c349adeffa01bd1387b8d7d26f2b66b00735e38e
Victor S <victor.sogamoso.05@gmail.com>
2025-03-10 11:02:31 -0500

    actualizacion de rutas

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,10 +1,13 @@
 import {Express} from "express";
 import express from "express";
 import uploadRoutes from "./uploadRoutes";
+import path from 'path';
 
 export const routes = (app: Express)=> {
     const router = express.Router();
+
     const frontendPath = path.join(__dirname, '../../frontend/dist');
+
     router.get("/", (req, res) => {
         res.send("API funcionando");
     });
